626177a37cd2d6845ffbed313402cdd4862b747a
pratikpatel <pratikpatelpp802@gmail.com>
2023-10-21 19:39:54 +0530

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,46 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-virtual-scroll
+      style="max-height: 300px;"
+      :items="heavyList"
+      separator
+      v-slot="{ item, index }"
+    >
+      <q-intersection
+        :key="index"
+        transition="flip-right"
+        class="example-item"
+      >
+        <q-item
+          dense
+        >
+          <q-item-section>
+            <q-item-label>
+              #{{ index }} - {{ item.label }}
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-intersection>
+    </q-virtual-scroll>
+  </q-page>
+</template>
+
+<script>
+import {defineComponent} from 'vue'
+const maxSize = 10000
+const heavyList = []
+
+for (let i = 0; i < maxSize; i++) {
+  heavyList.push({
+    label: 'Option ' + (i + 1)
+  })
+}
+export default defineComponent({
+  name: 'IndexPage',
+  data() {
+    return {
+      heavyList
+    }
+  },
+})
+</script>
